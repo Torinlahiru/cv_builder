c357fd97f9083485c1afcae39ce320d701d40dd3
Torin Lahiru <torinlahiru@gmail.com>
2025-12-31 06:14:22 +0530

    auth added

---
+++ cv-builder/resources/js/Components/CvPreview.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="bg-white p-6 shadow">
+        <h1 class="text-xl font-bold">{{ cv.personal?.name }}</h1>
+        <p>{{ cv.personal?.summary }}</p>
+    </div>
+</template>
+
+<script setup>
+defineProps({ cv: Object });
+</script>
---
+++ cv-builder/resources/js/Pages/CvBuilder.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="grid grid-cols-2 gap-4">
+        <CvForm @update="cv = $event" />
+        <CvPreview :cv="cv" />
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import CvForm from "@/Components/CvForm.vue";
+import CvPreview from "@/Components/CvPreview.vue";
+
+const cv = ref({});
+</script>
